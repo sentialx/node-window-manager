398912a3cb36c77cb6cbf9af948a1b85136433e0
Sential <sentialx@gmail.com>
2018-08-20 17:53:56 +0200

    Add getActiveWindow, moveWindow, getWindowBounds methods

--- install.js
+++ install.js
@@ -2,8 +2,5 @@ const os = require("os");
 const { spawn } = require("child_process");
 
 if (os.platform() === "win32") {
-  spawn.sync("npm", ["run", "build-win32"], {
-    input: "win32 detected. Build native module.",
-    stdio: "inherit"
-  });
+  const npm = spawn("npm.cmd", ["run", "build-win32"], { cwd: __dirname, stdio: "inherit" });
 }
