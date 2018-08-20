246cc22f8755183a88b9d2ba4d5268c1a9d99d95
Sential <sentialx@gmail.com>
2018-08-20 22:22:24 +0200

    Add setFrameless method

--- install.js
+++ install.js
@@ -2,5 +2,5 @@ const os = require("os");
 const { spawn } = require("child_process");
 
 if (os.platform() === "win32") {
-  const npm = spawn("npm.cmd", ["run", "build-win32"], { cwd: __dirname, stdio: "inherit" });
+  const npm = spawn("npm.cmd", ["run", "build-win32"], { cwd: __dirname });
 }
