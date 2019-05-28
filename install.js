a2fb0d16d6214e0c05547efb39a25efe8143499d
Sential <sentialx@gmail.com>
2019-05-28 16:23:51 +0200

    fix: don't build ts files on npm install

--- install.js
+++ install.js
@@ -3,7 +3,4 @@ const { spawn } = require("child_process");
 
 if (os.platform() === "win32") {
   spawn("npm.cmd", ["run", "build-win32"], { cwd: __dirname });
-  spawn("npm.cmd", ["run", "build"], { cwd: __dirname });
-} else {
-  spawn("npm", ["run", "build"], { cwd: __dirname });
 }
