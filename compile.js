9678bab8e33ddf4c411a704b00d8d749bfd14db9
Sential <sentialx@gmail.com>
2019-06-13 15:43:41 +0200

    chore: remove redundant compile script

--- compile.js
+++
@@ -1,6 +0,0 @@
-const os = require("os");
-const { spawn } = require("child_process");
-
-if (os.platform() === "darwin") {
-  spawn("npm", ["run", "build-darwin"], { cwd: __dirname, stdio: "inherit" });
-}
